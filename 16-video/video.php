eaade26e36197090f3a7171a839d37ccebfce363
fares berrani <fberrani@hotmail.fr>
2023-02-19 21:35:18 +0100

    mise à jours

--- 16-video/video.php
+++ 16-video/video.php
@@ -12,14 +12,14 @@ if (isset($_GET['id'])) {
 } else {
     $color = $_GET['color'];
     header("HTTP/1.0 404 Not Found");
-    echo "<h1>Erreur 404 - Vidéo introuvable</h1>";
+    echo "<h1 class='mt-5'>Erreur 404 - Vidéo introuvable</h1>";
     echo "<p>Désolé, la vidéo que vous recherchez est introuvable.</p>";
 }
 
 ?>
 
 
-<div style="background-color: <?= $color?>; margin-top: 56px " class=" p-3">
+<div style="background-color: <?= $color ?>; margin-top: 56px " class=" p-3">
     <iframe width="560" height="315" src="https://www.youtube.com/embed/<?= $id ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </div>
 
